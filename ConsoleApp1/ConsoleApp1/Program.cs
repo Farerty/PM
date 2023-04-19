83ba07c5d0fe559b0a0bb4fb61ee8f9fb7b83c97
Farerty <95239369+Farerty@users.noreply.github.com>
2023-04-19 08:59:39 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    struct Subjet
+    {
+        string name;
+        string famprep;
+        int semestr;
+    };
+    class PlanControl
+    {
+        int[] mas;
+        PlanControl(int size)
+        {
+            mas = new int[size];
+        }
+        public void Save()
+        {
+            StreamWriter str = new StreamWriter("Sinus.txt");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                str.WriteLine(mas[i]);
+            }
+            str.Close();
+        }
+
+        public void sortVoz()
+        {
+            for(int)
+            //сортир по семестру и по названию
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
