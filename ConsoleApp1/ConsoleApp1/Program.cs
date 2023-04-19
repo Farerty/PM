e1af49ca8b8ffb844db4f74f264b854b3efe5a76
Farerty <95239369+Farerty@users.noreply.github.com>
2023-04-19 10:58:17 +0300

    Add files via upload

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ConsoleApp1
@@ -11,36 +12,83 @@ namespace ConsoleApp1
     };
      class PlanControl
     {
-        int[] mas;
+        Subject[] mas;
         Subject sub = new Subject();
         public PlanControl(int size)
         {
-            mas = new int[size];
+            try
+            {
+                mas = new Subject[size];
+                strucFuller(size);
+            }
+            catch(Exception ex) { Console.WriteLine(ex.ToString()); }
+            
+        }
+        public void strucFuller(int size)
+        {
+            try
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine("Введите название дисциплины"+ ": "+i);
+                    sub.name = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите фамилию преподавателя" + ": " + i);
+                    sub.famprep = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите семестр: ");
+                    sub.semestr = Convert.ToInt32(Console.ReadLine());
+                    mas[i] = sub;
+                }
+            } 
+            catch(Exception ex) { Console.WriteLine(ex.ToString()); }
             
         }
         public void Save()
         {
-            StreamWriter str = new StreamWriter("Sinus.txt");
-            for (int i = 0; i < mas.Length; i++)
+            try
             {
-                str.WriteLine(mas[i]);
+                StreamWriter str = new StreamWriter("Sinus.txt");
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    str.WriteLine(mas[i]);
+                }
+                str.Close();
             }
-            str.Close();
+            catch(Exception ex) { Console.WriteLine(ex.ToString()); }
         }
 
         public void sortVoz()
         {
-            //for(int)
-            //сортир по семестру и по названию
+            try
+            {
+                Subject s;
+                for (int i = 0; i < mas.Length - 1; i++)
+                    for (int j = i; j >= 0; j--)
+                        if (String.Compare(mas[j].name, mas[j + 1].name) > 0 && mas[j].semestr > mas[j + 1].semestr) // функция Compare()
+                        {
+                            // поменять значения местами
+                            s = mas[j];
+                            mas[j] = mas[j + 1];
+                            mas[j + 1] = s;
+                        }
+                //сортир по семестру и по названию
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
         }
+            
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите размер массива: ");
-            int size = Convert.ToInt32(Console.ReadLine());
-            PlanControl pc = new PlanControl(size);
+            try
+            {
+                Console.WriteLine("Введите размер массива: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                PlanControl pc = new PlanControl(size);
+                pc.sortVoz();
+                pc.Save();
+            } 
+            catch(Exception ex) { Console.WriteLine(ex.ToString()); }
             
         }
     }
